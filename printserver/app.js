f0d0f889da3156e2eb4930c9543da3488badf85a
Richard Treichel <richard.treichel@hmhco.com>
2016-07-23 01:28:06 -0400

    Send sample report from client to server and echo it back

--- printserver/app.js
+++ printserver/app.js
@@ -1,28 +1,41 @@
-var express = require('express');
+// Node.js dependencies
 var http = require('http');
+var path = require('path');
+// Express dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Create an Express application object
 var app = express();
 var server = http.Server(app);
 var io = require('socket.io')(server);
 
+// Set the port
+var port = process.env.PORT || 3000;
+
+// Middleware for parsing 'Content-Type': 'application/json'
+app.use(bodyParser.json());
+
 // Serve index.html for all GET requests
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-// Enable CORS processing - Apparently, not needed for Socket.io's "Hello World"...?
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+// Log the request body and echo it back to the client
+app.post('/', function(req, res) {
+  console.log(req.body);
+  res.send(req.body);
+});
 
 // "Hello World" test for Socket.io
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
 });
 
-// Start listening on port 3000
-server.listen(3000);
+// Start listening on 'port'
+server.listen(port, function() {
+  console.log('Server listening on port ' + port);
+});
