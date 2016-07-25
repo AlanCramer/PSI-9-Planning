0cf017b85818facce770f7973211ff7cd938e729
bobhmh <robert.frieden@hmhco.com>
2016-07-25 16:34:31 -0400

    PDF download working

--- printing/nodeStuff/clientSide/controllers/reportsCtrl.js
+++ printing/nodeStuff/clientSide/controllers/reportsCtrl.js
@@ -7,36 +7,46 @@ routerApp.controller('reportsCtrl', function($scope, $http, $state) {
     $scope.goToNamedState = function(state) {
         $state.go(state);
     };
-
+    var data = [
+        { 'name':'Infosys Technologies',
+            'employees': 125000,
+            'headoffice': 'Bangalore'},
+        { 'name':'Cognizant Technologies',
+            'employees': 100000,
+            'headoffice': 'Bangalore'},
+        { 'name':'Wipro',
+            'employees': 115000,
+            'headoffice': 'Bangalore'},
+        { 'name':'Tata Consultancy Services (TCS)',
+            'employees': 150000,
+            'headoffice': 'Bangalore'},
+    ];
+    $scope.pdfAvailable = false;
     $scope.sendPost = function(reportName) {
-        var data = [
-            { 'name':'Infosys Technologies',
-                'employees': 125000,
-                'headoffice': 'Bangalore'},
-            { 'name':'Cognizant Technologies',
-                'employees': 100000,
-                'headoffice': 'Bangalore'},
-            { 'name':'Wipro',
-                'employees': 115000,
-                'headoffice': 'Bangalore'},
-            { 'name':'Tata Consultancy Services (TCS)',
-                'employees': 150000,
-                'headoffice': 'Bangalore'},
-        ];
-        /*var data = $.param({
-            json: JSON.stringify({
-                name: $scope.newName
-                })
-            });*/
         $scope.data = data;
-        $http.post("http://localhost:3000/?reportName=" + reportName, data).success(function(data, status) {
-            console.log('Send http post.  Data: ',data);
-            $scope.data = data;
-        })
+        $http.post("http://localhost:3000/?reportName=" + reportName, data).
+            then(function(response) {
+                console.log(response.status);
+                console.log(response.data);
+                $scope.fileName = response.data;
+                $scope.pdfAvailable = true;
+            }
+        )
     };
 });
 
 
+/*var data = $.param({
+ json: JSON.stringify({
+ name: $scope.newName
+ })
+ });*/
+
+/*
+ success(function(data, status) {
+ console.log('Send http post.  Data: ',data);
+ $scope.data = data;*/
+//saveAs(data,'testData.pdf');
 //ANother approach  from https://hello-angularjs.appspot.com/angularjs-http-service-ajax-post-json-data-code-example
 
 /*var helloAjaxApp = angular.module("helloAjaxApp", []);
--- printing/nodeStuff/server/app.js
+++ printing/nodeStuff/server/app.js
@@ -3,38 +3,25 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-
 //Load phantom
 var phantom = require('phantom');
-var sitepage = null;
 
-//Parse URL
+//This module puts a node wrapper around phatom to pdf conversion
+var pdf = require('html-pdf');
+
+//get URL parser
 var url =require('url');
-
 
 //Access file system
 var fs = require('fs');
 
 var dir = process.cwd();
+//could also use __dirname
 dir = path.join(dir,'psi-9-planning/printing/nodeStuff/server');
-//console.log(dir);
-//console.log(__dirname);
-
-
-//Enable CORS processing
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 
 var templateDir = path.join(dir, 'templates');
 
 var generatedPDFPath = path.join(dir,'GeneratedPDFs');
-
-//This module puts a node wrapper around phatom to pdf conversion
-var pdf = require('html-pdf');
 
 //Load html file synchronously via file system
 function getTemplate(reportName) {
@@ -47,75 +34,105 @@ function getFileNameForPDF(reportName) {
     return fileName;
 }
 
-//Test getTemplate
-//var html = getTemplate('lrs1');
+//Enable CORS processing
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //Set printer options per Phantom - see html-pdf website
 var options = { format: 'Letter' };
 
-// Create pdf (promise)
-/*pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
-    if (err) console.log(err);
-    console.log(res); // { filename: '/app/businesscard.pdf' }
-});*/
 
+//Add data to htmlTemplate
+function addDataToTemplate(data, html) {
+
+}
 
 //Encapulate above in function
 function createPDF(reportName, data) {
-
     var fullPathAndName = './templates/' + reportName + '.html';
-    //console.log(__dirname);
-    //console.log(fullPathAndName);
-
-   // var html = fs.readFileSync(fullPathAndName, 'utf8');
     var html = getTemplate(reportName);
+
+
+    addDataToTemplate(data, html);
+
     var timeStamp = new Date().getMilliseconds().toString();
     var fileNameForPDF = getFileNameForPDF(reportName + timeStamp); // './generatedPDFs/' + reportName + '.pdf';
+    console.log(fileNameForPDF);
     pdf.create(html, options).toFile(fileNameForPDF, function(err, res) {
-        if (err) return console.log(err);
-
-        console.log(res); // { filename: '/app/businesscard.pdf' }
+        if (err) {
+            //return console.log(err);
+        }
+        else {
+            //console.log(res);
+        }
     });
+    return fileNameForPDF;
 }
 
-//app.engine('.html', require('ejs').__express);
 app.set('views', __dirname);
 app.set('view engine', 'html');
 
-//var fixtureData = require('./fixture_data.json');
-
-//////////////////////////////////////Stuff to handle post from client
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-
-// POST http://localhost:8080/api/users
-// parameters sent with
+// Handle post from client
 app.post('/', function(req, res) {
     //console.log(req);
     var parts = url.parse(req.url,true);
     var reportName = parts.query.reportName;
-    //console.log(parts);
-    //console.log(parts.body);
     var data = res.req.body;
-    //console.log('Report name: ' + reportName + '  Data: ' + data);
 
-    res.send('Report: '+ reportName + " requested at " + Date());
-
-    createPDF(reportName, data);
-    //res.send(user_id + ' ' + token + ' ' + geo);
+    var fileName = createPDF(reportName, data);
+    res.send(fileName);
+    //res.download(pdfFullPath);
+    console.log(req);
 });
-/////////////////////////////////////////////////
 
-
-app.get('/', function(req, res) {
+app.get('/getPDF', function(req, res) {
     var parts = url.parse(req.url,true);
-    //createPDF(1, 2);
-    console.log(parts.query.reportName);
-    res.send('hello world'+ Date() + '  ReportName: ' + parts.query.reportName);
+    res.download("/Users/bobhmh/WebstormProjects/nodeTest/PSI-9-Planning/printing/nodeStuff/server/GeneratedPDFs/irc118.pdf");
 
 });
 
+var renderPdf = function(session, cb) {
+    var page;
+
+    try {
+        session.createPage(function(_page) {
+            page = _page;
+            // ...
+            var file = '/tmp/file.pdf';
+            page.render(file, function() {
+                page.close();
+                page = null;
+                return cb(null, file);
+            });
+        });
+    }
+    catch(e) {
+        try {
+            if (page != null) {
+                page.close(); // try close the page in case it opened but never rendered a pdf due to other issues
+            }
+        } catch(e) {
+            // ignore as page may not have been initialised
+        }
+        return cb('Exception rendering pdf:' + e.toString());
+    }
+};
+
+
+//Starting listening
 app.listen(3000);
 console.log('listening on port 3000');
+
+
+// Create pdf (promise)
+/*pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
+ if (err) console.log(err);
+ console.log(res); // { filename: '/app/businesscard.pdf' }
+ });*/
