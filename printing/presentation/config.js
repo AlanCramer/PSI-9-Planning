cdc3855affa05b451a1731780337671e092563e9
alan.cramer <alan.cramer@hmhco.com>
2016-11-02 07:58:22 -0400

    presentation update

--- printing/presentation/config.js
+++ printing/presentation/config.js
@@ -8,7 +8,9 @@ var pages = [
         'Teacher, Leader, Student logins',
         'WAC Report Example: multiple charts, multiple tables',
         'Intellify Report Printing',
-        'Zeplin View: the print specification'
+        'Zeplin View: the print specification',
+        '----',
+        'Comment: Not going to get there with only paged-media CSS'
     ] },
     { title: 'Our Technology Stack', bullets: [
         'Our Client: scent-client',
@@ -18,18 +20,33 @@ var pages = [
         'RequireJS, Gulp, Grunt, ++',
         'Intellify',
     ] },
-    { title: 'Some HTML to PDF Technologies', bullets: [
-        'PhantomJS',
-        'PDFKit',
-        'PDFMake',
-        'wkhtmltopdf',
-        'Comment: Consistency Requirement implies Server Side Solution'
+    { title: 'Some PDF Generation Technologies', bullets: [
+        '<b>PhantomJS</b> - Headless Browser with PDF Export (HTML to PDF)',
+        '<b>wkhtmltopdf</b> - Headless Browser with PDF Export (HTML to PDF)',
+        '<b>PDFKit</b> - scripting language to create PDF',
+        '<b>PDFMake</b> - PDFKit plus more',
+        'Others - <ul style=\'font-size:smaller\'><li>Snappy (wkhtmltopdf wrapper)</li>' +
+            '<li>DocRaptor (commercial service)</li>' +
+            '<li>wicked_pdf (ruby scripting)</li></ul>',
+        'Comment: Consistency Requirement implies Server Side Solution',
+        'Comment: Code Reuse implies Headless Browser Approach'
+    ] },
+    { title: 'Server', bullets: [
+        'AWS',
+        'Node vs JBOSS',
+        'Operations owns Scaling and Load Balancing',
+        'Server side process management'
     ] },
     { title: 'Proof Of Concept', bullets: [
         'Start with PhantomJS: somewhat proven',
         'Stand Alone Pages',
+        ''
     ] },
-    { title: 'Demo of Current State', bullets: [] }
+    { title: 'Demo of Current State', bullets: [] },
+    { title: 'Final Comments', bullets: [
+        'Code Reuse: add Route for printed version of report',
+        'Security: TLSP vs Independent, SSO'
+    ] },
 ];
 
 pages[0].bullets = pages.map(function (d) { return d.title; });
