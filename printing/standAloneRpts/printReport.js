09441e9e09b39c78e363c08c797200f8e63cc720
Chuck Doucette <Charles.Doucette@hmhco.com>
2016-11-28 14:42:18 -0500

    1) changed printReport.js to capture data from one page and use it in the header. 2) updated the style of the header to use proper syntax (attribute: value)

--- printing/standAloneRpts/printReport.js
+++ printing/standAloneRpts/printReport.js
@@ -5,6 +5,22 @@ var CookieJar = "cookiejar.json";
 
 var page = require('webpage').create();
 
+var versionText = "unknown";
+
+var getTitle = function () {
+  var title = 'Workshop Asssessment Class Report';
+  return title;
+};
+
+var getSubTitle = function () {
+  var subTitle = 'Class ' + versionText + ' - Read 180';
+  return subTitle;
+}
+
+var getLogoFile = function () {
+  var logoFile = 'images/d3Report/work_shop_logo.svg';
+  return logoFile;
+};
 
 page.paperSize = {
 	//viewportSize: { width: 960, height: 1200 },
@@ -17,12 +33,12 @@ page.paperSize = {
     	height: '50px',
     	contents: phantom.callback(function (pageNum, numPages) {
 
-        var title = 'Workshop Asssessment Class Report';
-        var subTitle = 'Class 1 - Read 180';
-        var logoFile = 'images/d3Report/work_shop_logo.svg';
+        var title = getTitle();
+        var subTitle = getSubTitle();
+        var logoFile = getLogoFile();
 
         var result = '<div>';
-        result = result + '<div id="title" style="text-align: left; font-size 24px;">' + title + '</div>';
+        result = result + '<div id="title" style="text-align: left; font-size: 24px;"><h1>' + title + '</h1></div>';
         result = result + '<div class="rightbox"><img src="' + logoFile + '"/></div>';
         result = result + '<div id="subtitle">' + subTitle + '</div>';
         result = result + '</div>';
@@ -47,15 +63,27 @@ if(fs.isFile(CookieJar)) {
     });
 }
 
-
-page.open(url, function(status) {
-
-	console.log('here');
-	console.log('typeof window.setTimeout: ' + (typeof window.setTimeout));
-	window.setTimeout(function() {
-		console.log('hello!');
-	    page.render('report.pdf')
-	    phantom.exit();
-	}, 2000);
-
+page.open("http://phantomjs.org/", function(status) {
+    if (status === "success") {
+        console.log("loaded http://phantomjs.org with status: " + status);
+        page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js", function() {
+            versionText = page.evaluate(function() {
+                // lastest version on the web
+                var version = $("span.version").text();
+                console.log("$(\"span.version\").text() -> " + version);
+                return version;
+              });
+              page.open(url, function(status) {
+                console.log('here');
+                console.log('typeof window.setTimeout: ' + (typeof window.setTimeout));
+                window.setTimeout(function() {
+                  console.log('hello!');
+                  page.render('report.pdf')
+                  phantom.exit();
+              }, 2000);
+            });
+        });
+    } else {
+      phantom.exit(1);
+    }
 });
