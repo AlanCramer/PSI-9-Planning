55f2f5582f3cab8e886acd4a64d8f8fa6a4c2b38
Chuck Doucette <Charles.Doucette@hmhco.com>
2017-01-06 13:30:30 -0500

    Refactored copy-pdf-to-s3 to make it closer to the lambda function handler

--- lambda/copy-pdf-to-s3.js
+++ lambda/copy-pdf-to-s3.js
@@ -1,30 +1,55 @@
 'use strict';
 
 console.log('Loading script and its dependencies...');
-const aws = require('aws-sdk');
+const AWS = require('aws-sdk');
 const parseuri = require('parseuri');
 const fs = require('fs');
 const request = require('request');
 const path = require('path');
+var S3 = null;
 
-const getArgs = function () {
+const getProcessArgs = function () {
     let args = {};
-    let argv = process.argv;
-    args.fileUrl = decodeURIComponent(argv[2]);
-    args.bucketName = argv[3];
-    args.accessKeyId= argv[4];
-    args.secretAccessKey = argv[5];
+    args.encodedFileUrl = process.argv[2];
+    args.bucketName = process.argv[3];
+    args.accessKeyId= process.argv[4];
+    args.secretAccessKey = process.argv[5];
     return args;
 };
 
+const copyUrlToS3 = function (args) {
+  console.log("called copyUrlToS3 with args: " + JSON.stringify(args));
+  let fileUrl = decodeURIComponent(args.encodedFileUrl);
+  console.log("fileUrl: " + fileUrl);
+  let parsedUrl = parseuri(fileUrl);
+  let destFileName = parsedUrl.file;
+  console.log("parseduri(" + fileUrl + ").file : " + destFileName);
+  const tmpDir = "/tmp";
+  const PATH_SEPARATOR = '/';
+  let destFilePath = tmpDir + PATH_SEPARATOR + destFileName;
+  console.log("destFilePath = " + destFilePath);
+
 const download = function(url, dest, cb) {
+    if (!url) {
+      if (cb) {
+        return cb('No url provided');
+      }
+    }
+
+    if (!dest) {
+      if (cb) {
+        return cb('No dest provided');
+      }
+    }
+
     console.log("Downloading from url: " + url + " to destination file path: " + dest);
     let sendReq = request.get(url);
 
     // verify response code
     sendReq.on('response', function(response) {
-        if (response.statusCode !== 200) {
-            return cb('Response status was ' + response.statusCode);
+        if (response.statusCode !== 200 && cb) {
+            let errMsg = 'Response status was ' + response.statusCode;
+            return cb(errMsg);
         }
     });
 
@@ -38,7 +63,6 @@ const download = function(url, dest, cb) {
     });
 
     let file = fs.createWriteStream(dest);
-
     sendReq.pipe(file);
 
     file.on('finish', function() {
@@ -54,58 +78,61 @@ const download = function(url, dest, cb) {
     });
 };
 
-var args = getArgs();
-var fileUrl = args.fileUrl;
-console.log("fileUrl: " + fileUrl);
-var parsedUrl = parseuri(fileUrl);
-var destFileName = parsedUrl.file;
-console.log("parseduri(" + fileUrl + ").file : " + destFileName);
-var tmpDir = "/tmp";
-const PATH_SEPARATOR = '/';
-var destFilePath = tmpDir + PATH_SEPARATOR + destFileName;
-console.log("destFilePath = " + destFilePath);
-var bucket = new aws.S3({accessKeyId: args.accessKeyId,
-                        secretAccessKey: args.secretAccessKey,
-                        params: {Bucket: args.bucketName}
-                      });
-
 const upload = function() {
 
-  console.log("uploading file: " + destFilePath + " to bucket: " + args.bucketName);
-  let uploadFile = fs.createReadStream(destFilePath);
-  let fileName = path.parse(destFilePath).base;
-  console.log("parsed file name: '" + fileName + "'");
-  let params = {
-      Key: fileName,
-      ContentType: uploadFile.type,
-      Body: uploadFile,
-      ACL: 'public-read'
-  };
-  if (!bucket)
-  {
-    console.log("No bucket available to upload");
+  if (!S3) {
+    console.log("No S3 available to upload!");
   }
-  else
-  {
-    bucket.putObject(params, function (err, data) {
+  else if (!args.bucketName) {
+    console.log("No target bucket name provided!");
+  }
+  else {
+    console.log("uploading file: " + destFilePath + " to bucket: " + args.bucketName);
+    let uploadFile = fs.createReadStream(destFilePath);
+    let fileName = path.parse(destFilePath).base;
+    console.log("parsed file name: '" + fileName + "'");
+    let params = {
+        Bucket: args.bucketName,
+        Key: fileName,
+        ContentType: uploadFile.type,
+        Body: uploadFile,
+        ACL: 'public-read'
+    };
+    let awsRequest = S3.putObject(params, function (err, data) {
       if (err)
       {
         console.log("Failed to upload file: " + JSON.stringify(err));
       }
       else {
         console.log("Succcessfully uploaded file - got data: " + JSON.stringify(data));
-      }
+      };
     });
-  }
+    return awsRequest;
+  };
 }
 
 const doneDownloadingCallback = function(msg) {
-  console.log("callback - file is downloaded");
-  if (msg)
-  {
+  let awsRequest = null;
+  if (msg) {
     console.log("callback(msg) == " + msg);
   }
-  upload();
+  else {
+    awsRequest = upload();
+  }
+  return awsRequest;
 };
 
-download(fileUrl, destFilePath, doneDownloadingCallback);
+  let awsRequest = download(fileUrl, destFilePath, doneDownloadingCallback);
+};
+
+let args = getProcessArgs();
+if (args.accessKeyId && args.secretAccessKey)
+{
+  console.log("Setting up default credentials to AWS");
+  let credentials = new AWS.Credentials({accessKeyId: args.accessKeyId, secretAccessKey: args.secretAccessKey});
+  AWS.config.credentials = credentials;
+  delete args.accessKeyId;
+  delete args.secretAccessKey;
+}
+S3 = new AWS.S3();
+copyUrlToS3(args);
