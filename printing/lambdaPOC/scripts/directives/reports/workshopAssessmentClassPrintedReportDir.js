fab6233beebd5499bd9964ef4ec435a6a2a7c35e
Chuck Doucette <Charles.Doucette@hmhco.com>
2017-02-15 09:07:33 -0500

    Improved data flow in stand alone printed report. Unfortunately, a) the web page is still not showing both bar graphs, but if you try to print the page, they are shown. b) the tables are not being shown.

--- printing/lambdaPOC/scripts/directives/reports/workshopAssessmentClassPrintedReportDir.js
+++ printing/lambdaPOC/scripts/directives/reports/workshopAssessmentClassPrintedReportDir.js
@@ -1,29 +1,60 @@
 'use strict';
-      app.directive('workshopAssessmentClassPrintedReport',
-        function () {
+      app.directive('workshopAssessmentClassPrintedReport',['$timeout',
+        function ($timeout) {
+
+          var directive = {
+            restrict: 'E',
+            scope : true,
+            replace: true,
+            link: linker,
+            templateUrl: 'scripts/directives/templates/reports/workshopAssessmentClassPrintedReport.html',
+            controller: wacController
+          };
+          console.log("created: workshopAssessmentClassPrintedReport");
+          return directive;
+
+          function getData (scope) {
+
+              var model = scope.model = {
+                  wkshpAsmtReportInit: false
+              };
+
+              $timeout(function init() {
+                  var dataPromise = instantiateModelData(scope.modelUrl);
+
+                  dataPromise.then(function (data) {
+                      console.log("Promise was resolved with data.");
+                      model.titleData = data.titleModel;
+                      model.toolBar = data.toolbarModel;
+                      model.summaryData = data.summaryModel;
+                      model.barChartData = data.barChartModel;
+                      model.tableData = data.tableModel;
+                      model.teacherTableModel = data.teacherTableModel;
+                      model.tableData.noDataPanel =
+                          data.tableModel.data.length === 0   &&
+                          data.tableModel.levelA.length === 0 &&
+                          data.tableModel.levelB.length === 0;
+                      model.activeLrsTableModel = data.lrsTableModelA;
+                      model.lrsTableModelA = data.lrsTableModelA;
+                      model.lrsTableModelB = data.lrsTableModelB;
+                      model.activeLrsTeacherTableModel = data.lrsTeacherTableModelA;
+                      model.lrsTeacherTableModelA = data.lrsTeacherTableModelA;
+                      model.lrsTeacherTableModelB = data.lrsTeacherTableModelB;
+
+                      // Needed to detect when to show the teacher scored items table
+//                      var assessmentName = workshopAssessmentApiSrv.getCurrentAssessmentName() || '';
+//                      scope.isEndOfWorkshop = assessmentName.indexOf('End of Workshop') !== -1;
+
+                      model.wkshpAsmtReportInit = true;
+                  }).catch(function (reason) {
+                    console.error("Promise failed due to: %s", JSON.stringify(reason));
+                  });
+              });
+          }
 
           function wacController ($scope) {
             console.log("called wacController");
-            $scope.model.wkshpAsmtReportInit = true;
-            var model = $scope.model;
-            var data = $scope.data;
-            model.titleData = data.titleModel;
-            model.toolBar = data.toolbarModel;
-            model.summaryData = data.summaryModel;
-            model.barChartData = data.barChartModel;
-            model.tableData = data.tableModel;
-            model.teacherTableModel = data.teacherTableModel;
-            model.tableData.noDataPanel =
-                data.tableModel.data.length === 0   &&
-                data.tableModel.levelA.length === 0 &&
-                data.tableModel.levelB.length === 0;
-            model.activeLrsTableModel = data.lrsTableModelA;
-            model.lrsTableModelA = data.lrsTableModelA;
-            model.lrsTableModelB = data.lrsTableModelB;
-            model.activeLrsTeacherTableModel = data.lrsTeacherTableModelA;
-            model.lrsTeacherTableModelA = data.lrsTeacherTableModelA;
-            model.lrsTeacherTableModelB = data.lrsTeacherTableModelB;
-
+            getData($scope);
             $scope.levelSelectedA = true;
             $scope.teacherLevelSelectedA = true;
           }
@@ -35,15 +66,4 @@
               $('#pocModal').modal('show');
             };
           }
-
-          var directive = {
-            restrict: 'E',
-            scope : true,
-            replace: true,
-            link: linker,
-            templateUrl: 'scripts/directives/reports/templates/reports/workshopAssessmentClassPrintedReport.html',
-            controller: wacController
-          };
-          console.log("created: workshopAssessmentClassPrintedReport");
-          return directive;
-        });
+        }]);
