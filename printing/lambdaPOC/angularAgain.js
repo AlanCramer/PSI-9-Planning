26a34d553a50666dde6a7596c2a6f502139fc47d
Richard Treichel <richard.treichel@hmhco.com>
2017-03-17 11:24:27 -0400

    Configure  service to use html5Mode, allowing the model data URL to be passed as a query parameter

--- printing/lambdaPOC/angularAgain.js
+++ printing/lambdaPOC/angularAgain.js
@@ -1,25 +1,39 @@
 'use strict';
 
-// Register a new Angular module named 'wac', which has no dependencies
+// Register a new Angular module named 'wac' along with its dependencies (None right now)
 var app = angular.module('wac', []);
+
+// Configure the $location service to use html5Mode. Needed in order to
+// use $location.search() to obtain query params from modern URLs that
+// respect the HTML 5 History API.
+app.config(['$locationProvider',
+    function($locationProvider) {
+        $locationProvider.html5Mode(true);
+    }
+]);
 
 // Set up an injectable object of relative paths organized by category
 app.constant('paths', {
-    data: {
-        wacData: 'modelData/workshopAssessmentMergedModel.json'
-    },
     templates: {
         wacPrintReport: 'scripts/directives/templates/reports/wacPrintReport.html',
         lrsTable: 'scripts/directives/visualizations/templates/lrsTable.html'
     }
 });
 
-app.controller('wacCtrl', ['$scope', '$http', 'paths',
-    function($scope, $http, paths) {
-        $http.get(paths.data.wacData).then(function(response) {
-            $scope.wacData = (response && response.data) || {};
-            console.log('wacData: ', $scope.wacData);
-        });
+app.controller('wacCtrl', ['$scope', '$http', '$location', 'paths',
+    function($scope, $http, $location, paths) {
+        // Identify the URL that should be queried for WAC model data
+        var queryParams = $location.search();
+        $scope.modelUrl = queryParams['modelUrl'];
+        console.log('modelUrl: ', $scope.modelUrl);
+
+        // If a model URL was found, issue a GET request against that URL to obtain the data
+        if ($scope.modelUrl) {
+            $http.get($scope.modelUrl).then(function(response) {
+                $scope.wacData = (response && response.data) || {};
+                console.log('wacData: ', $scope.wacData);
+            });
+        }
     }
 ]);
 
