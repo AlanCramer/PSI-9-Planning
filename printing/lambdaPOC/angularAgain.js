684268e920cf3c0134e4f1c2b876518a0ae6d979
Richard Treichel <richard.treichel@hmhco.com>
2017-03-22 12:06:26 -0400

    Finish updating directive files so they can be used across Angular modules, and finish the standalone WAC Angular app test

--- printing/lambdaPOC/angularAgain.js
+++ printing/lambdaPOC/angularAgain.js
@@ -58,40 +58,5 @@ app.directive('wacPrintReport', ['paths',
 addSharedReportServiceToModule('wac');
 addLRSTableDirectiveToModule('wac');
 addHorizBarChartDirectiveToModule('wac');
-
-// app.directive('lrsTable', ['paths',
-//     function(paths) {
-//         return {
-//             restrict: 'E',
-//             scope: {
-//                 tableModel: '='
-//             },
-//             templateUrl: paths.templates.lrsTable
-//         };
-//     }
-// ]);
-
-// // Copied from http://stackoverflow.com/questions/17417607/angular-ng-bind-html-and-directive-within-it?noredirect=1&lq=1
-// app.directive('compile', ['$compile',
-//     function ($compile) {
-//         return function(scope, element, attrs) {
-//             scope.$watch(
-//                 function(scope) {
-//                   // watch the 'compile' expression for changes
-//                   return scope.$eval(attrs.compile);
-//                 },
-//                 function(value) {
-//                   // when the 'compile' expression changes
-//                   // assign it into the current DOM
-//                   element.html(value);
-
-//                   // compile the new DOM and link it to the current
-//                   // scope.
-//                   // NOTE: we only compile .childNodes so that
-//                   // we don't get into infinite loop compiling ourselves
-//                   $compile(element.contents())(scope);
-//                 }
-//             );
-//         };
-//     }
-// ]);
+addReportTitleDirectiveToModule('wac');
+addReportSummaryDirectiveToModule('wac');
